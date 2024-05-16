12cf5b67d3e9443bea87c3cf79df65b2e2ed3a9a
malixds <masik2003_410@mail.ru>
2024-05-16 14:13:39 +0300

    delete icon at acum page

--- resources/views/pages/accum.blade.php
+++ resources/views/pages/accum.blade.php
@@ -9,14 +9,6 @@
     </div>
     <div id="solution" class="pt-36">
         <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-sky-500">
-                <path fill-rule="evenodd"
-                    d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"
-                    clip-rule="evenodd"></path>
-                <path fill-rule="evenodd"
-                    d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"
-                    clip-rule="evenodd"></path>
-            </svg>
             <div
                 class="space-y-6 lg:flex justify-between text-gray-600 md:flex md:flex-row md:gap-6 md:space-y-0 lg:gap-12">
                 <div class="md:7/12 lg:w-1/2">
